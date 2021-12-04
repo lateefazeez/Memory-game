dc232e3f78c266640b4a53f382b33029a70abe91
Lateef Azeez <codelate18@gmail.com>
2021-12-04 13:33:13 -0700

    automatically start a new game and show turns

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -22,6 +22,9 @@ function App() {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -51,6 +54,11 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
+  // Automatically restart the game
+  useEffect(() => {
+    shuffleCard()
+  }, [])
+
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
@@ -74,6 +82,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
